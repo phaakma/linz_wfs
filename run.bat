09897b320922d983d2e53612df7b42dc36ac81d9
Paul Haakma <phaakma@gmail.com>
2024-06-30 13:02:55 +1200

    Remove unused line and reference logfile variable.

--- run.bat
+++ run.bat
@@ -1,12 +1,10 @@
 @echo off
 setlocal
-cls
 
 SET "python_path=C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\pythonw.exe"
 SET "python_file=LINZ_WFS.py"
 
-rem Define the path to the temporary log file
-set tempfile="%CD%\logs\temp_output.log"
+rem Define the path to the log file
 set logfile="%CD%\logs\%python_file%_last_run.log"
 
 rem Delete the existing log file if it exists
@@ -24,6 +22,6 @@ ECHO.
 REM Ensure logs folder exists
 if not exist "%CD%\logs" mkdir "%CD%\logs"
 
-"%python_path%" "%CD%\%python_file%" %* 1> "%CD%\logs\%python_file%_last_run.log" 2>&1
+"%python_path%" "%CD%\%python_file%" %* 1> %logfile% 2>&1
 
 endlocal
