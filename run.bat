2b61d8e26c53b960a44a3e68ccb89d0d2f0997bd
Paul Haakma <phaakma@gmail.com>
2024-07-01 14:01:46 +1200

    Move logfile to main directory. Make it append rather than overwrite.

--- run.bat
+++ run.bat
@@ -3,12 +3,7 @@ setlocal
 
 SET "python_path=C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\pythonw.exe"
 SET "python_file=LINZ_WFS.py"
-
-rem Define the path to the log file
-set logfile="%CD%\logs\%python_file%_last_run.log"
-
-rem Delete the existing log file if it exists
-if exist %logfile% del %logfile%
+set logfile="%CD%\LINZ_WFS_last_run.log"
 
 ECHO ========================================================
 ECHO SCRIPT PATH = %CD%
@@ -19,9 +14,6 @@ ECHO arguments   = %*
 ECHO ========================================================
 ECHO.
 
-REM Ensure logs folder exists
-if not exist "%CD%\logs" mkdir "%CD%\logs"
-
-"%python_path%" "%CD%\%python_file%" %* 1> %logfile% 2>&1
+"%python_path%" "%CD%\%python_file%" %* 1>> %logfile% 2>&1
 
 endlocal
