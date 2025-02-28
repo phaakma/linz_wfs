e0cacd5f377880d55d96e874c6d615fdfee62033
Paul Haakma <phaakma@gmail.com>
2025-02-28 21:05:17 +1300

    Run.bat batch file logs have to go to separate files so multiple processes don't fail trying to write to the same log file.

--- run.bat
+++ run.bat
@@ -3,7 +3,14 @@ setlocal
 
 SET "python_path=C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\pythonw.exe"
 SET "python_file=LINZ_WFS.py"
-set logfile="%CD%\LINZ_WFS_batch_logs.log"
+set "log_dir=%CD%\logs"
+set "timestamp=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%"
+set "logfile=%log_dir%\LINZ_WFS_batch_logs_%timestamp%.log"
+
+REM Create the logs directory if it doesn't exist
+if not exist "%log_dir%" (
+    mkdir "%log_dir%"
+)
 
 ECHO ========================================================
 ECHO SCRIPT PATH = %CD%
